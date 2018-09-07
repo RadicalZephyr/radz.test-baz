3ed893f773a9616998df7a2233ddbcb41f523c32
Geoff Shannon <geoffpshannon@gmail.com>
2018-09-07 02:22:14 -0700

    Add qux-number function

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject radz.test/baz "1.0"
+(defproject radz.test/baz "1.1"
   :description ""
   :url "https://github.com/radicalzephyr/radz.test-baz"
   :scm {:url "https://github.com/radicalzephyr/radz.test-baz"}
--- src/radz/test/baz.clj
+++ src/radz/test/baz.clj
@@ -1,3 +1,6 @@
 (ns radz.test.baz)
 
 (def number 1)
+
+(defn qux-number []
+  radz.test.qux/number)
